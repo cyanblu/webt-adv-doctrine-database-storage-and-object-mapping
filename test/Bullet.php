e01fd8b1ceb6d64ea613343414e972a20570018c
Christian Widauer <9198@htl.rennweg.at>
2023-05-24 12:30:31 +0200

    Mitschreibedatein

---
+++ test/Bullet.php
@@ -0,0 +1,50 @@
+<?php
+
+// src/Bullet.php
+namespace Dreni\DoctrineOrm;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'bullets')]
+class Bullet
+{
+    #[ORM\Id]
+    #[ORM\Column(type: 'integer')]
+    #[ORM\GeneratedValue]
+    private int|null $id = null;
+    #[ORM\Column(type: 'string')]
+    private string $name;
+
+    /**
+     * @return int|null
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+}
